23126779630e788f233c3232daba41d6a3c7464a
Mehmood,Majid (init) <Majid.Mehmood@init.de>
2022-11-05 11:05:13 +0100

    add kitty ranger

--- archlinux/provision-i3.sh
+++ archlinux/provision-i3.sh
@@ -1,6 +1,3 @@
 #/bin/sh
 
-
-# pacman --noconfirm -Sy
-# pacman --noconfirm -S firefox dmenu nitrogen i3 xorg-server xorg-xinit
 echo "exec i3" >> ~/.xinitrc
--- archlinux/provision.sh
+++ archlinux/provision.sh
@@ -8,11 +8,13 @@ pacman --noconfirm -Syu
 pacman --noconfirm -R virtualbox-guest-utils-nox
 # feel free to add/remove packages as you need
 pacman --noconfirm -Syu \
-    base-devel net-tools vim wget git unzip openssh bash-completion \
+    base-devel net-tools vim wget git unzip openssh bash-completion 
+    apache \
+    kitty \
+    ranger \
     xorg xorg-xinit \
     xterm \
     xf86-video-intel \
     i3 slim dmenu \
-    firefox \
-    apache 
+    firefox
 
